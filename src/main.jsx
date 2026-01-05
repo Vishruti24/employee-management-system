4a8d92e7071f5b87813ded4740a8171ff7b33c72
Vishruti24 <vishu2412004@gmail.com>
2026-01-05 09:04:20 +0530

    1

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import "./index.css";
-// import { EmployeeProvider } from "./context/EmployeeContext";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <EmployeeProvider>
-//       <App />
-//     </EmployeeProvider>
-//   </React.StrictMode>
-// );
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
